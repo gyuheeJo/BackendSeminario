776081fc5315a6c763b886872a7eba4fe5fa3e19
GyuHee Jo <gyuheejo@igg.com.co>
2025-07-06 15:22:45 -0500

    cors changed

--- main/src/main/java/com/main/config/SecurityConfig.java
+++ main/src/main/java/com/main/config/SecurityConfig.java
@@ -74,7 +74,7 @@ public class SecurityConfig {
         CorsConfigurationSource corsConfigurationSource() {
                 CorsConfiguration configuration = new CorsConfiguration();
                 configuration.addAllowedOrigin("http://localhost:5173");
-                configuration.addAllowedOrigin("https://integraservicios.ngrok.app");
+                configuration.addAllowedOrigin("https://frontend-seminario.vercel.app");
                 configuration.addAllowedMethod("*");
                 configuration.addAllowedHeader("*");
                 configuration.setAllowCredentials(true);
